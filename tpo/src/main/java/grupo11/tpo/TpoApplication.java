02235c89e92bfb11a7e9e9f0875c84c55f78e9ec
DESKTOP-1UO2SI0\mrfra <mrfranco97@gmail.com>
2024-11-02 18:29:34 -0300

    Se implemento la capacidad de agregar reservas al huesped con el guardado correcto de fechas

--- tpo/src/main/java/grupo11/tpo/TpoApplication.java
+++ tpo/src/main/java/grupo11/tpo/TpoApplication.java
@@ -2,6 +2,7 @@ package grupo11.tpo;
 
 import grupo11.tpo.entity.Hotel;
 import grupo11.tpo.entity.Huesped;
+import grupo11.tpo.entity.Reserva;
 import grupo11.tpo.repository.HotelRepository;
 import grupo11.tpo.service.HotelService;
 import grupo11.tpo.service.HuespedService;
@@ -10,6 +11,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.Date;
+import java.util.Scanner;
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -28,12 +35,25 @@ public class TpoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Huesped huespedjava = new Huesped("Juan23","Monroe 123","45564455","juan@algo.com");
+		/*Huesped huespedjava = new Huesped("Juan23","Monroe 123","45564455","juan@algo.com");
 		huespedService.guardarHuesped(huespedjava);
 		for (Huesped huesped : huespedService.obtenerHuespedes()) {
 			System.out.println(huesped.getName()+" "+huesped.getId());
 		}
-		System.out.println(huespedService.modificarHuesped("67266104452d354a468c8583","Carlos","","123456","Carlos@gmail.com"));
+		System.out.println(huespedService.modificarHuesped("67266104452d354a468c8583","Carlos","","123456","Carlos@gmail.com"));*/
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ingrese fecha inicio (formato: yyyy-MM-dd):");
+		String inicio = scanner.nextLine();
+		LocalDate fecha_ini = LocalDate.parse(inicio, DateTimeFormatter.ISO_LOCAL_DATE);
+		System.out.println("Fecha ingresada: " + fecha_ini);
+		System.out.println("Ingrese fecha fin (formato: yyyy-MM-dd):");
+		String fin = scanner.nextLine();
+			// Convierte la cadena a LocalDate
+		LocalDate fecha_fin = LocalDate.parse(fin, DateTimeFormatter.ISO_LOCAL_DATE);
+		System.out.println("Fecha ingresada: " + fecha_fin);
+		Reserva reserva = new Reserva(fecha_ini,fecha_fin,666L);
+		huespedService.agregarReservaAlHuesped("672661fb8d993413df08456a",reserva);
 
 	}
 
--- tpo/src/main/java/grupo11/tpo/entity/Reserva.java
+++ tpo/src/main/java/grupo11/tpo/entity/Reserva.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+import java.time.LocalDate;
 import java.util.Date;
 
 @Data
@@ -16,12 +17,12 @@ public class Reserva {
     @MongoId
     private String id;
 
-    private Date fecha_inicio;
-    private Date fecha_final;
+    private LocalDate fecha_inicio;
+    private LocalDate fecha_final;
     private Long id_habitacion;
     private boolean confirmacion;
 
-    public Reserva(Date fecha_inicio,Date fecha_final,Long id_habitacion){
+    public Reserva(LocalDate fecha_inicio,LocalDate fecha_final,Long id_habitacion){
         this.fecha_inicio=fecha_inicio;
         this.fecha_final=fecha_final;
         this.id_habitacion=id_habitacion;
