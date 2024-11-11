c6b8599a52aa9bd4a01e03269e0100c82eab4b6c
DESKTOP-1UO2SI0\mrfra <mrfranco97@gmail.com>
2024-11-11 16:11:53 -0300

    Se agrego las consultas que estaban en otra rama

--- tpo/src/main/java/grupo11/tpo/component/OperacionesMenu.java
+++ tpo/src/main/java/grupo11/tpo/component/OperacionesMenu.java
@@ -257,8 +257,8 @@ public class OperacionesMenu {
             System.out.println("2. Buscar Reservas por Fecha");
             System.out.println("3. Buscar Reservas por Numero de Confirmacion (ID)");
             System.out.println("4. Consultar Reservas por Huesped");
-            System.out.println("5. Consultar Detalles de Hotel"); // Pendiente
-            System.out.println("6. Consultar Amenities de Habitacion"); // Pendiente
+            System.out.println("5. Consultar Detalles de Hotel");
+            System.out.println("6. Consultar Amenities de Habitacion");
             System.out.println("0. Volver al menú principal");
 
             option = scanner.nextInt();
@@ -295,10 +295,44 @@ public class OperacionesMenu {
                     reservasHuesped.forEach(System.out::println); // Mostrar reservas del huésped
                     break;
                 case 5:
-
+                    System.out.print("Seleccion el hotel... ");
+                    for(Hotel hotel : hotelService.obtenerHoteles()){
+                        System.out.print(hotel.getId()+". "+hotel.getName());
+                        System.out.println();
+                    }
+                    Long seleccion = scanner.nextLong();
+                    scanner.nextLine();
+                    Hotel hotel_seleccionado=hotelService.buscarHotelPorId(seleccion);
+                    System.out.println("-----------------------Detalles:---------------- ");
+                    System.out.println("Nombre: "+hotel_seleccionado.getName());
+                    System.out.println("Direccion: "+hotel_seleccionado.getAddress());
+                    System.out.println("Mail: "+hotel_seleccionado.getMail());
+                    System.out.println("Telefono: "+hotel_seleccionado.getPhone());
+                    System.out.println("Ubicacion: "+hotel_seleccionado.getLocation());
+                    System.out.println("--------------------------------------------------- ");
                     break;
                 case 6:
-
+                    System.out.println("Seleccione el hotel...");
+                    for(Hotel hoteles: hotelService.obtenerHoteles())
+                    {
+                        System.out.print(hoteles.getId()+". "+hoteles.getName());
+                        System.out.println();
+                    }
+                    Long hotel_sel=scanner.nextLong();
+                    Hotel hotel_selec=hotelService.buscarHotelPorId(hotel_sel);
+                    for(Habitacion habitaciones:hotel_selec.getHabitaciones()){
+                        System.out.print(habitaciones.getId()+". "+habitaciones.getTipo());
+                        System.out.println();
+                    }
+                    Long habitacion_sel=scanner.nextLong();
+                    scanner.nextLine();
+                    Habitacion habitacion_selec=habitacionService.obtenerHabitacionporId(habitacion_sel);
+                    System.out.println("-------------------------Amenities--------------------------");
+                    for(Amenity amenties : habitacion_selec.getAmenities()){
+                        System.out.println("-"+amenties.getName());
+                        System.out.println();
+                    }
+                    System.out.println("------------------------------------------------------------");
                     break;
                 case 0:
                     System.out.println("Volviendo al menú de huespedes...");
